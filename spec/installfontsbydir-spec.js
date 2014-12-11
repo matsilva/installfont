7e61e5c696f7fe3edbfae05a432b1d71b6fb6fc2
matsilva <askmatsilva@gmail.com>
2014-12-10 19:56:18 -0500

    fixed single font file params and methods

--- spec/installfontsbydir-spec.js
+++ spec/installfontsbydir-spec.js
@@ -3,32 +3,62 @@ var path = require('path');
 var assert = require("assert");
 var fs = require('fs');
 
+describe('Install all fonts in specified directory with options', function(){
+  it('should install fonts and delete fonts in original directory', function(done){
+    this.timeout(0);
+    if(!fs.existsSync('./spec/temp-fonts-2')) fs.mkdirSync('./spec/temp-fonts-2');
+    var tempFiles = [];
+    var tempFilesByName =[];
+    var fontDir = fs.readdirSync('./spec/spec-fonts2');
+
+    for (var i = 0; i < fontDir.length; i++) {
+      tempFiles.push(path.join('./spec/temp-fonts-2', fontDir[i]));
+      tempFilesByName.push(fontDir[i]);
+      fs.createReadStream(path.join('./spec/spec-fonts2', fontDir[i])).pipe(fs.createWriteStream(path.join('./spec/temp-fonts-2', fontDir[i])));
+    }
+    var opts = {};
+    opts.removeFonts = true;
+    installfont('./spec/temp-fonts-2', function(err){
+      if(err) console.log(err, err.stack);
+
+      for(var j = 0; j < tempFilesByName.length; j++){
+          assert.equal(true, fs.existsSync(path.join('c:/Windows/Fonts', tempFilesByName[j])));
+      }
+
+      setTimeout(function () {
+        for(var i = 0; i < tempFiles.length; i++){
+            assert.equal(false, fs.existsSync(tempFiles[i]));
+        }
+        done();
+      }, 2000);
+    }, opts);
+  });
+});
+
+
 describe('Install all fonts in specified directory', function(){
   it('should install fonts and leave fonts in original directory', function(done){
-    this.timeout(10000);
+    this.timeout(0);
     if(!fs.existsSync('./spec/temp-fonts-1')) fs.mkdirSync('./spec/temp-fonts-1');
     var tempFiles = [];
     var tempFilesByName =[];
     var fontDir = fs.readdirSync('./spec/spec-fonts');
 
-    console.log(fontDir)
     for (var i = 0; i < fontDir.length; i++) {
       tempFiles.push(path.join('./spec/temp-fonts-1', fontDir[i]));
       tempFilesByName.push(fontDir[i]);
-      fs.createReadStream(path.join('./spec/spec-fonts', fontDir[i])).pipe(fs.createWriteStream(path.join('./spec/temp-fonts-1', fontDir[i])))
+      fs.createReadStream(path.join('./spec/spec-fonts', fontDir[i])).pipe(fs.createWriteStream(path.join('./spec/temp-fonts-1', fontDir[i])));
     }
 
     installfont('./spec/temp-fonts-1', function(err){
-      if(err) console.log(err, err.stack)
-        for(var i = 0; i < tempFiles.length; i++){
-            assert.equal(true, fs.existsSync(tempFiles[i]))
-        }
-        for(var i = 0; i < tempFilesByName.length; i++){
-            assert.equal(true, fs.existsSync(path.join('c:/Windows/Fonts', tempFilesByName[i])));
-        }
-        done();
-    })
-
-
-  })
-})
+      if(err) console.log(err, err.stack);
+      for(var i = 0; i < tempFiles.length; i++){
+          assert.equal(true, fs.existsSync(tempFiles[i]));
+      }
+      for(var j = 0; j < tempFilesByName.length; j++){
+          assert.equal(true, fs.existsSync(path.join('c:/Windows/Fonts', tempFilesByName[j])));
+      }
+      done();
+    });
+  });
+});
---
+++ spec/installfontsbyfile-spec.js
@@ -0,0 +1,48 @@
+var installfont = require('../lib/installfont');
+var path = require('path');
+var assert = require("assert");
+var fs = require('fs');
+
+describe('Install single specified font', function(){
+  it('should install specified font and leave it in original directory', function(done){
+    this.timeout(0);
+    if(!fs.existsSync('./spec/temp-fonts-3')) fs.mkdirSync('./spec/temp-fonts-3');
+    var tempFiles = [];
+    var tempFilesByName =[];
+
+    fs.createReadStream('./spec/spec-fonts3/Martyric_PersonalUse.ttf')
+      .pipe(fs.createWriteStream('./spec/temp-fonts-3/Martyric_PersonalUse.ttf'));
+
+    installfont('./spec/temp-fonts-3/Martyric_PersonalUse.ttf', function(err){
+      if(err) console.log(err, err.stack);
+      assert.equal(true, fs.existsSync('./spec/temp-fonts-3/Martyric_PersonalUse.ttf'));
+      assert.equal(true, fs.existsSync('c:/Windows/Fonts/Martyric_PersonalUse.ttf'));
+      done();
+    });
+  });
+});
+
+describe('Install single specified font', function(){
+  it('should install specified font and delete it in original directory', function(done){
+    this.timeout(0);
+    if(!fs.existsSync('./spec/temp-fonts-3')) fs.mkdirSync('./spec/temp-fonts-3');
+    var tempFiles = [];
+    var tempFilesByName =[];
+
+    fs.createReadStream('./spec/spec-fonts3/Silver Bellybutton Ring Reg.ttf')
+      .pipe(fs.createWriteStream('./spec/temp-fonts-3/Silver Bellybutton Ring Reg.ttf'));
+
+    var opts = {};
+    opts.removeFonts = true;
+
+    installfont('./spec/temp-fonts-3/Silver Bellybutton Ring Reg.ttf', function(err){
+      if(err) console.log(err, err.stack);
+      assert.equal(true, fs.existsSync('c:/Windows/Fonts/Martyric_PersonalUse.ttf'));
+      setTimeout(function () {
+        assert.equal(false, fs.existsSync('./spec/temp-fonts-3/Silver Bellybutton Ring Reg.ttf'));
+        done();
+      }, 500);
+
+    }, opts);
+  });
+});
