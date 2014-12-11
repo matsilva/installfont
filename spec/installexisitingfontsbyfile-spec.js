f288d4039b7110ccad50aab49ae7b1f893482fb6
matsilva <askmatsilva@gmail.com>
2014-12-11 11:10:58 -0500

    added tests for pre-exsiting fonts, passin err instead of silent fail

---
+++ spec/installexisitingfontsbyfile-spec.js
@@ -0,0 +1,52 @@
+var installfont = require('../lib/installfont');
+var path = require('path');
+var assert = require("assert");
+var fs = require('fs');
+
+describe('Try Install single existing specified font', function(){
+  it('should return err object in callback and leave font in original directory', function(done){
+    this.timeout(0);
+    if(!fs.existsSync('./spec/temp-fonts-3')) fs.mkdirSync('./spec/temp-fonts-3');
+    var tempFiles = [];
+    var tempFilesByName =[];
+
+    fs.createReadStream('./spec/spec-fonts3/Martyric_PersonalUse.ttf')
+      .pipe(fs.createWriteStream('./spec/temp-fonts-3/Martyric_PersonalUse.ttf'));
+
+    installfont('./spec/temp-fonts-3/Martyric_PersonalUse.ttf', function(err){
+      //if(err) console.log(err, err.stack);
+      assert.equal(true, err !== 'undefined');
+      assert.equal('Font already installed', err.message);
+      assert.equal(true, fs.existsSync('./spec/temp-fonts-3/Martyric_PersonalUse.ttf'));
+      assert.equal(true, fs.existsSync('c:/Windows/Fonts/Martyric_PersonalUse.ttf'));
+      done();
+    });
+  });
+});
+
+describe('Try Install single existing specified font with options', function(){
+  it('should return err object in callback and remove font in original directory', function(done){
+    this.timeout(0);
+    if(!fs.existsSync('./spec/temp-fonts-3')) fs.mkdirSync('./spec/temp-fonts-3');
+    var tempFiles = [];
+    var tempFilesByName =[];
+
+    fs.createReadStream('./spec/spec-fonts3/Silver Bellybutton Ring Reg.ttf')
+      .pipe(fs.createWriteStream('./spec/temp-fonts-3/Silver Bellybutton Ring Reg.ttf'));
+
+    var opts = {};
+    opts.removeFonts = true;
+
+    installfont('./spec/temp-fonts-3/Silver Bellybutton Ring Reg.ttf', function(err){
+      //if(err) console.log(err, err.stack);
+      assert.equal(true, err !== 'undefined');
+      assert.equal('Font already installed', err.message);
+      assert.equal(true, fs.existsSync('c:/Windows/Fonts/Martyric_PersonalUse.ttf'));
+      setTimeout(function () {
+        assert.equal(false, fs.existsSync('./spec/temp-fonts-3/Silver Bellybutton Ring Reg.ttf'));
+        done();
+      }, 500);
+
+    }, opts);
+  });
+});
---
+++ spec/installexistingfontsbydir-spec.js
@@ -0,0 +1,67 @@
+var installfont = require('../lib/installfont');
+var path = require('path');
+var assert = require("assert");
+var fs = require('fs');
+
+describe('Try Install all fonts in specified directory that already exist with opts param', function(){
+  it('should return err object in callback and delete fonts in original directory', function(done){
+    this.timeout(0);
+    if(!fs.existsSync('./spec/temp-fonts-2')) fs.mkdirSync('./spec/temp-fonts-2');
+    var tempFiles = [];
+    var tempFilesByName =[];
+    var fontDir = fs.readdirSync('./spec/spec-fonts2');
+
+    for (var i = 0; i < fontDir.length; i++) {
+      tempFiles.push(path.join('./spec/temp-fonts-2', fontDir[i]));
+      tempFilesByName.push(fontDir[i]);
+      fs.createReadStream(path.join('./spec/spec-fonts2', fontDir[i])).pipe(fs.createWriteStream(path.join('./spec/temp-fonts-2', fontDir[i])));
+    }
+    var opts = {};
+    opts.removeFonts = true;
+    installfont('./spec/temp-fonts-2', function(err){
+      //if(err) console.log(err, err.stack);
+      assert.equal(true, err !== 'undefined');
+      assert.equal('All fonts already installed', err.message);
+      for(var j = 0; j < tempFilesByName.length; j++){
+          assert.equal(true, fs.existsSync(path.join('c:/Windows/Fonts', tempFilesByName[j])));
+      }
+
+      setTimeout(function () {
+        for(var i = 0; i < tempFiles.length; i++){
+            assert.equal(false, fs.existsSync(tempFiles[i]));
+        }
+        done();
+      }, 2000);
+    }, opts);
+  });
+});
+
+
+describe('Try Install all fonts in specified directory that already exist', function(){
+  it('should return err object in callback and leave fonts in original directory', function(done){
+    this.timeout(0);
+    if(!fs.existsSync('./spec/temp-fonts-1')) fs.mkdirSync('./spec/temp-fonts-1');
+    var tempFiles = [];
+    var tempFilesByName =[];
+    var fontDir = fs.readdirSync('./spec/spec-fonts');
+
+    for (var i = 0; i < fontDir.length; i++) {
+      tempFiles.push(path.join('./spec/temp-fonts-1', fontDir[i]));
+      tempFilesByName.push(fontDir[i]);
+      fs.createReadStream(path.join('./spec/spec-fonts', fontDir[i])).pipe(fs.createWriteStream(path.join('./spec/temp-fonts-1', fontDir[i])));
+    }
+
+    installfont('./spec/temp-fonts-1', function(err){
+      //if(err) console.log(err, err.stack);
+      assert.equal(true, err !== 'undefined');
+      assert.equal('All fonts already installed', err.message);
+      for(var i = 0; i < tempFiles.length; i++){
+          assert.equal(true, fs.existsSync(tempFiles[i]));
+      }
+      for(var j = 0; j < tempFilesByName.length; j++){
+          assert.equal(true, fs.existsSync(path.join('c:/Windows/Fonts', tempFilesByName[j])));
+      }
+      done();
+    });
+  });
+});
